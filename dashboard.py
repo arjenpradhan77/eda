88faf784d277743448f06c75e36e7ec809fe2df8
arjenpradhan <124348717+arjenpradhan77@users.noreply.github.com>
2025-03-16 15:16:27 +0545

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,103 @@
+from altair.vegalite.v5.display import here
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import streamlit as st
+import os
+import warnings
+warnings.filterwarnings('ignore')
+
+st.set_page_config(page_title='Data Visualization', page_icon=':tokyo_tower:', layout = 'wide')
+
+st.title(':tokyo_tower: LTE Daily Report')
+
+df = pd.read_excel(r'Site Status Report_March_16_2025.xlsx', sheet_name= 'Site Report')
+df.index = df.index + 1
+
+
+# Total site status count
+total = df['Site Status'].value_counts()
+
+
+# Site Status Based on Network
+network_df = df.groupby('Network')['Site Status'].value_counts().reset_index(name= 'Count')
+network_df.index = range(1, len(network_df) + 1)
+
+
+# Total Sites Down By Network
+network_down = network_df[(network_df['Site Status'] == 'Down')]
+network_down.index = range(1, len(network_down) + 1)
+
+
+# Site Status Based on Region
+region_df = df.groupby('Region')['Site Status'].value_counts().reset_index(name='Count')
+region_df.index = range(1, len(region_df) + 1)
+
+
+# Total Sites Down By Region
+region_down = region_df[region_df['Site Status'] == 'Down']
+region_down.index = range(1, len(region_down) + 1)
+
+
+# Site Status Based on Province
+province_df = df.groupby('Province')['Site Status'].value_counts().reset_index(name='Count')
+province_df.index = range(1, len(province_df) + 1)
+
+
+# Total Sites Down on Province
+province_down = province_df[province_df['Site Status'] == 'Down']
+province_down.index = range(1, len(province_down) + 1)
+
+
+# Filtering down type with province name
+# selected_down_type = st.radio('Select Site Down Features', ['Down', 'Site Down. Manually Locked', 'Site Down. Not Working due to Error'])
+# selected_down_pro = st.slider('Select Province', min_value= 1, max_value=7)
+# filtered_down_type = df[(df['Site Report'] == selected_down_type) & (df['Province'] == selected_down_pro)]
+# filtered_down_type = filtered_down_type.drop(columns=['Network', 'Region','Site Status'])
+
+
+# Filtering up type with province name
+# selected_up_type = st.selectbox('Select Site Up with Cell Not Working Feature', ['Up', 'Site Up; 1 Cells Manually Locked', 'Site Up; 1 Cells not Working due to Alarm', 'Site Up; 2 Cells Manually Locked', 'Site Up; 2 Cells not Working due to Alarm', 'Site Up; 3 Cells Manually Locked', 'Site Up; 3 Cells not Working due to Alarm'])
+# selected_up_pro = st.slider('Select Province', min_value= 1, max_value=7, value= 3)
+# filtered_up_type = df[(df['Site Report'] == selected_up_type) & (df['Province'] == selected_up_pro)]
+# filtered_up_type = filtered_up_type.drop(columns=['Network', 'Region', 'Site Status'])
+
+
+
+
+st.sidebar.markdown('# Info')
+selected_category = st.sidebar.radio('Select Category', ['Overview', 'Network', 'Region', 'Province', 'Down Feature', 'Up Feature'])
+if selected_category == 'Overview':
+    st.write('Total Sites Overview', total)
+
+elif selected_category == 'Network':
+    st.write('Total Sites Down By Network Type', network_down)
+    st.write('Site Status Based on Networks', network_df)
+
+elif selected_category == 'Region':
+    st.write('Total Sites Down on Region', region_down )
+    st.write('Sites Status Based on Region', region_df )
+
+elif selected_category == 'Province':
+    st.write('Total Sites Down on Province', province_down)
+    st.write('Site Status Based on Province', province_df)
+
+elif selected_category == 'Down Feature':
+    selected_down_type = st.radio('Select Site Down Features', ['Down', 'Site Down. Manually Locked', 'Site Down. Not Working due to Error'])
+    selected_down_pro = st.slider('Select Province', min_value= 1, max_value=7)
+    filtered_down_type = df[(df['Site Report'] == selected_down_type) & (df['Province'] == selected_down_pro)]
+    filtered_down_type = filtered_down_type.drop(columns=['Network', 'Region','Site Status'])
+    filtered_down_type.index = range(1, len(filtered_down_type) + 1)
+    st.write('Sites Down By Features & Province', filtered_down_type )
+
+elif selected_category == 'Up Feature':
+    selected_up_type = st.selectbox('Select Site Up with Cell Not Working Feature', ['Up', 'Site Up; 1 Cells Manually Locked', 'Site Up; 1 Cells not Working due to Alarm', 'Site Up; 2 Cells Manually Locked', 'Site Up; 2 Cells not Working due to Alarm', 'Site Up; 3 Cells Manually Locked', 'Site Up; 3 Cells not Working due to Alarm'])
+    selected_up_pro = st.slider('Select Province', min_value= 1, max_value=7, value= 3)
+    filtered_up_type = df[(df['Site Report'] == selected_up_type) & (df['Province'] == selected_up_pro)]
+    filtered_up_type = filtered_up_type.drop(columns=['Network', 'Region', 'Site Status'])
+    filtered_up_type.index = range(1, len(filtered_up_type) + 1)
+    st.write('Sites Up By Features & Province', filtered_up_type)
+
+
+
