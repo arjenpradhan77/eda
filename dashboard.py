7a746dc528e597329b864753fc4ec95d8eadc38d
arjenpradhan <124348717+arjenpradhan77@users.noreply.github.com>
2025-03-17 14:00:24 +0545

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -9,8 +9,10 @@ import warnings
 warnings.filterwarnings('ignore')
 
 st.set_page_config(page_title='Data Visualization', page_icon=':tokyo_tower:', layout = 'wide')
+current_date = datetime.today().date()
 
-st.title(':tokyo_tower: LTE Daily Report')
+st.header(':tokyo_tower: LTE Daily Report')
+st.write(current_date)
 
 df = pd.read_excel('Site Status Report_March_16_2025.xlsx', sheet_name= 'Site Report')
 df.index = df.index + 1
