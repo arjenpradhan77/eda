d87d17164eb89f12363fe461ea81776710d350b6
arjenpradhan <124348717+arjenpradhan77@users.noreply.github.com>
2025-05-16 10:08:35 +0545

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -18,7 +18,7 @@ st.header(':tokyo_tower: LTE Daily Report')
 st.write(current_date)
 
 # URL of the file on Dropbox (direct download link)
-dropbox_url = "https://www.dropbox.com/scl/fi/1b7ucdj7tqly544nqt3a2/Site_Report_May_15_2025.xlsx?rlkey=3d41aosq48hqbap8dm3hssyld&st=i8x14iyl&dl=1"  # Replace with your actual URL
+dropbox_url = "https://www.dropbox.com/scl/fi/0q14rar5hy6kvsqjp2rew/Site_Report_May_16_2025.xlsx?rlkey=84uzqigww9frwd0fuyv43udkc&st=yw91f24n&dl=1"  # Replace with your actual URL
 
 # Function to download and read the file, caching the download and reading process
 @st.cache_data
